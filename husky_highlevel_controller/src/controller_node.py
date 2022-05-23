487628b82d2f547568fe83f9c4a19e0799bb208e
Benjamin Wagner <benjamin.wagner@htwg-konstanz.de>
2022-05-23 13:36:01 +0200

    cleanup

--- husky_highlevel_controller/src/controller_node.py
+++ husky_highlevel_controller/src/controller_node.py
@@ -16,7 +16,7 @@ class Controller():
         laser_queue_size = rospy.get_param("/queue_size")
         laser_topic_name = rospy.get_param("/topic_name")
 
-        # Create Subscirber
+        # Create Subscriber
         rospy.Subscriber(laser_topic_name, LaserScan, callback=self.laser_callback, queue_size=laser_queue_size)
       
         # Create publisher
@@ -27,7 +27,7 @@ class Controller():
         self.current_angle = 0
         self.current_position = None
 
-        # tf2 buffer, listener and broadcaster
+        # tf2 buffer and listener
         self.tfBuffer = tf2_ros.Buffer()
         self.tflistener = tf2_ros.TransformListener(self.tfBuffer)
 
@@ -39,6 +39,7 @@ class Controller():
         angle_min = filtered_msg.angle_min
         angle_increment = filtered_msg.angle_increment
 
+        # return when empty scan 
         if not len(filtered_msg.ranges):
             return
 
@@ -47,12 +48,15 @@ class Controller():
         angle_rad = angle_min + angle_increment * mid_index
         angle_deg = self.rad_to_deg(angle_rad)
 
-        x, y = self.polar_to_karth(range, angle_rad)
+        # calculate x,y coordinates of nearest points (laser_frame)
+        laser_x, laser_y = self.polar_to_karth(range, angle_rad)
+        # transform coordinates from laser frame to odom
+        odom_x, odom_y = self.tf_laser_to_odom(laser_x, laser_y)
 
-        self.tf_laser_to_odom(x,y)
+        if odom_x and odom_y :
+            self.publish_marker(odom_x, odom_y, "odom")
 
-        #self.publish_marker(x,y, "base_laser")
-
+        # orient and drive Robo to nearest point
         if abs(angle_deg) > 5:
             self.send_drive_cmd(angle= -np.sign(angle_deg) * 0.2)
         elif range > 0.5:
@@ -75,7 +79,7 @@ class Controller():
         marker.action = marker.ADD
         marker.scale.x = 0.2
         marker.scale.y = 0.2
-        marker.scale.z = 0.2
+        marker.scale.z = 1.0
         marker.color.a = 1.0
         marker.color.r = 1.0
         marker.color.g = 0
@@ -86,6 +90,21 @@ class Controller():
         marker.pose.position.z = 0
         self.marker_publisher.publish(marker)
 
+    def tf_laser_to_odom(self, laser_x, laser_y):
+        laser_point = PointStamped()
+        laser_point.header.frame_id = "base_laser"
+        laser_point.header.stamp = rospy.Time(0)
+        laser_point.point.x= laser_x
+        laser_point.point.y= laser_y
+        laser_point.point.z= 0.0
+
+        try:
+            p = self.tfBuffer.transform(laser_point, "odom", timeout=rospy.Duration(10.0))
+            return p.point.x, p.point.y
+
+        except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
+            return None, None
+
     def get_vector3(self, x=0, y=0, z=0):
         vector = Vector3()
         vector.x = float(x)
@@ -93,27 +112,6 @@ class Controller():
         vector.z = float(z)
 
         return vector
-
-    def tf_laser_to_odom(self, laser_x, laser_y):
-        try:
-            #trans = self.tfBuffer.lookup_transform('base_laser', 'odom', rospy.Time())
-
-            laser_point = PointStamped()
-            laser_point.header.frame_id = "base_laser"
-            laser_point.header.stamp = rospy.Time()
-            laser_point.point.x= laser_x
-            laser_point.point.y= laser_y
-            laser_point.point.z= 0.0
-            laser_point.header.stamp = rospy.Time(0)
-
-            #p = self.tfBuffer.transform(laser_point, "odom")
-            p = self.tfBuffer.transform(laser_point, "odom", timeout=rospy.Duration(10.0))
-
-            self.publish_marker(p.point.x, p.point.y, "odom")
-        except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
-            pass
-
-   
 
     def send_drive_cmd(self, speed=0, angle=0):
         twist_msg = Twist()
